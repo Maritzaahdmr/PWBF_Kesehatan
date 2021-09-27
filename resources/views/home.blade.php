ef8c959c5163128cbf1ece8ac4f38f1b7aff24d4
Maritzaahdmr <ahnadamaramaritza@gmail.com>
2021-09-27 22:31:58 +0700

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -165,7 +165,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('frontend\AdminLTE-master') }}/dist/img/aku.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('frontend\AdminLTE-master') }}/dist/img/fm1.jpeg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Maritza Lutfiyah</a>
